06bf6dce31964625b40a33524ec69f95087a5656
congjing <1529797261@qq.com>
2015-07-21 16:25:51 +0800

    修改密码长度

--- app/src/main/java/com/feifan/bp/password/ResetPasswordFragment.java
+++ app/src/main/java/com/feifan/bp/password/ResetPasswordFragment.java
@@ -88,14 +88,33 @@ public class ResetPasswordFragment extends BaseFragment implements View.OnClickL
     TextWatcher mTextWatcher = new TextWatcher() {
         @Override
         public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            if (mNewPwd.getText().length() > Constants.PASSWORD_MAX_LENGTH) {
-                Utils.showShortToast(R.string.error_message_text_password_length_max, Gravity.CENTER);
-            }
+
         }
 
         @Override
         public void onTextChanged(CharSequence s, int start, int before, int count) {
+            Editable editable = mNewPwd.getText();
+            int len = editable.length();
+            if (len > Constants.PASSWORD_MAX_LENGTH) {
+                if (len == Constants.PASSWORD_MAX_LENGTH+1){
+                    Utils.showShortToast(R.string.error_message_text_password_length_max, Gravity.CENTER);
+                }
+                int selEndIndex = Selection.getSelectionEnd(editable);
+                String str = editable.toString();
+                //截取新字符串
+                String newStr = str.substring(0, Constants.PASSWORD_MAX_LENGTH);
+                mNewPwd.setText(newStr);
+                editable = mNewPwd.getText();
 
+                //新字符串的长度
+                int newLen = editable.length();
+                //旧光标位置超过字符串长度
+                if (selEndIndex > newLen) {
+                    selEndIndex = editable.length();
+                }
+                //设置新光标所在的位置
+                Selection.setSelection(editable, selEndIndex);
+            }
         }
 
         @Override
