7c47268fc9085c191b8044033612a65d92e9d59c
maning <maninghere@163.com>
2015-08-26 17:40:30 +0800

    Turst all https CA.

--- app/src/main/java/com/feifan/bp/net/DefaultHttpProcessor.java
+++ app/src/main/java/com/feifan/bp/net/DefaultHttpProcessor.java
@@ -1,10 +1,21 @@
 package com.feifan.bp.net;
 
 import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.http.AndroidHttpClient;
+import android.os.Build;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HttpClientStack;
+import com.android.volley.toolbox.HttpStack;
+import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.Volley;
+
+import java.io.File;
 
 /**
  * Created by maning on 15/7/27.
@@ -23,7 +34,7 @@ public class DefaultHttpProcessor implements HttpProcessor, RequestQueue.Request
     }
 
     private DefaultHttpProcessor(Context context) {
-        mQueue = Volley.newRequestQueue(context);
+        mQueue = newRequestQueue(context, null);
         mQueue.addRequestFinishedListener(this);
         mQueue.start();
     }
@@ -38,6 +49,33 @@ public class DefaultHttpProcessor implements HttpProcessor, RequestQueue.Request
     @Override
     public void process(BaseRequest request) {
         mQueue.add(request);
+    }
+
+
+    public static RequestQueue newRequestQueue(Context context, HttpStack stack) {
+        File cacheDir = new File(context.getCacheDir(), "wandabp");
+        String userAgent = "wandabp/0";
+
+        try {
+            String network = context.getPackageName();
+            PackageInfo queue = context.getPackageManager().getPackageInfo(network, 0);
+            userAgent = network + "/" + queue.versionCode;
+        } catch (PackageManager.NameNotFoundException var6) {
+
+        }
+
+        if(stack == null) {
+            if(Build.VERSION.SDK_INT >= 9) {
+                stack = new HttpsUrlStack();
+            } else {
+                stack = new HttpClientStack(AndroidHttpClient.newInstance(userAgent));
+            }
+        }
+
+        BasicNetwork network1 = new BasicNetwork((HttpStack)stack);
+        RequestQueue queue1 = new RequestQueue(new DiskBasedCache(cacheDir), network1);
+        queue1.start();
+        return queue1;
     }
 
 }
