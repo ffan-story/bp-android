2217b22c32aa6f09813ba41e6724c4b2d3466ac0
zgqwork <congjing05@163.com>
2015-10-29 17:13:17 +0800

    add message refresh listview
    Change-Id: Iaffa6e6286d8389e6f61cc3aa423e4eaebe3ac6d

--- app/src/main/java/com/feifan/bp/home/MessageFragment.java
+++ app/src/main/java/com/feifan/bp/home/MessageFragment.java
@@ -1,29 +1,47 @@
 package com.feifan.bp.home;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.android.volley.toolbox.ImageLoader;
 import com.feifan.bp.OnFragmentInteractionListener;
 import com.feifan.bp.R;
 import com.feifan.bp.base.BaseFragment;
+import com.feifan.bp.util.LogUtil;
+import com.feifan.bp.widget.LoadingMoreListView;
+import com.feifan.bp.widget.OnLoadingMoreListener;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link MessageFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class MessageFragment extends BaseFragment {
+import java.util.ArrayList;
+import java.util.List;
+
+import bp.feifan.com.refresh.PtrClassicFrameLayout;
+import bp.feifan.com.refresh.PtrDefaultHandler;
+import bp.feifan.com.refresh.PtrFrameLayout;
+import bp.feifan.com.refresh.PtrHandler;
+
+public class MessageFragment extends BaseFragment implements OnLoadingMoreListener{
     private static final String TAG = MessageFragment.class.getSimpleName();
 
-    private OnFragmentInteractionListener mListener;
+//    private ImageLoader mImageLoader;
+    private PtrClassicFrameLayout mPtrFrame;
+
+
+
+    LoadingMoreListView mListView;
+    private List<String> mList =new ArrayList<>();
+    private Adapter mAdapter;
 
 
     public static MessageFragment newInstance() {
@@ -51,25 +69,150 @@ public class MessageFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_message, container, false);
-    }
+//        mImageLoader = ImageLoaderFactory.create(getContext());
 
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        try {
-            mListener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
+        final View contentView = inflater.inflate(R.layout.refresh_listview, null);
+
+        mListView = (LoadingMoreListView) contentView.findViewById(R.id.rotate_header_list_view);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position >= 0) {
+
+
+                }
+            }
+        });
+
+        mAdapter = new Adapter(getActivity());
+        for (int i=0;i<7;i++){
+            mList.add("i="+i);
         }
+        mListView.setAdapter(mAdapter);
+        mListView.setOnLoadingMoreListener(this);
+        mPtrFrame = (PtrClassicFrameLayout) contentView.findViewById(R.id.rotate_header_list_view_frame);
+        mPtrFrame.setLastUpdateTimeRelateObject(this);
+        mPtrFrame.setPtrHandler(new PtrHandler() {
+            @Override
+            public void onRefreshBegin(PtrFrameLayout frame) {
+                updateData();
+            }
+
+            @Override
+            public boolean checkCanDoRefresh(PtrFrameLayout frame, View content, View header) {
+                return PtrDefaultHandler.checkContentCanBePulledDown(frame, content, header);
+            }
+        });
+        // the following are default settings
+        mPtrFrame.setResistance(1.7f);
+        mPtrFrame.setRatioOfHeaderHeightToRefresh(1.2f);
+        mPtrFrame.setDurationToClose(200);
+        mPtrFrame.setDurationToCloseHeader(1000);
+        // default is false
+        mPtrFrame.setPullToRefresh(false);
+        // default is true
+        mPtrFrame.setKeepHeaderWhenRefresh(true);
+        mPtrFrame.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mPtrFrame.autoRefresh();
+            }
+        }, 100);
+        return contentView;
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
+
     }
 
+    /**
+     * 跟新
+     */
+    protected void updateData() {
+        LogUtil.e(TAG,"UPdate data!!!!!");
+        mPtrFrame.refreshComplete();
+    }
+
+    /**
+     * 加载更多
+     */
+    @Override
+    public void onLoadingMore() {
+        // 控制脚布局隐藏
+        //mListView.hideFooterView();
+    }
+
+
+    class Adapter extends BaseAdapter {
+
+        private final LayoutInflater mInflater;
+        Context context ;
+        public Adapter(Context context) {
+            this.context = context;
+            mInflater = LayoutInflater.from(context);
+        }
+
+        @Override
+        public int getCount() {
+            return mList == null ? 0 : mList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            final ViewHolder holder;
+            if (convertView == null) {
+                convertView = mInflater.inflate(R.layout.item_message,parent, false);
+                holder = ViewHolder.findAndCacheViews(convertView);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            if (false) {
+                holder.mImgRedPoint.setVisibility(View.VISIBLE);
+                holder.mTvMessageTitle.setTextColor(context.getResources().getColor(R.color.font_color_66));
+            } else {
+                holder.mImgRedPoint.setVisibility(View.VISIBLE);//Gone
+                holder.mTvMessageTitle.setTextColor(context.getResources().getColor(R.color.feed_back_color));
+            }
+
+            holder.mTvMessageTitle.setText("关于万达召开中秋活动相关通知，关于万达召开中秋活动相关通知");
+            holder.mTvMessageTime.setText( "2015-3-3 20：00");
+
+            return convertView;
+        }
+    }
+
+    private static class ViewHolder {
+
+        private ImageView mImgRedPoint;
+
+        private TextView mTvMessageTitle;
+        private TextView mTvMessageTime;
+
+
+        public static ViewHolder findAndCacheViews(View view) {
+            ViewHolder holder = new ViewHolder();
+            holder.mImgRedPoint = (ImageView) view.findViewById(R.id.img_message_redpoint);
+
+            holder.mTvMessageTitle = (TextView) view.findViewById(R.id.tv_message_title);
+            holder.mTvMessageTime = (TextView) view.findViewById(R.id.tv_message_time);
+
+
+            view.setTag(holder);
+            return holder;
+        }
+    }
 }
