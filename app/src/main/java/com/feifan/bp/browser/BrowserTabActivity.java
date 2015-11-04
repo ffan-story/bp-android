1165766816dafc1f350f27ffae43c3db4dd40d58
RogerFong13 <fangke3@wanda.cn>
2015-11-04 10:58:22 +0800

    browserTabActivity add menu
    Change-Id: Ibc46d64d7e20e4ac80ab4bd15578c8253f793e6b

--- app/src/main/java/com/feifan/bp/browser/BrowserTabActivity.java
+++ app/src/main/java/com/feifan/bp/browser/BrowserTabActivity.java
@@ -6,20 +6,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.view.Gravity;
 import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.PopupWindow;
 
 import com.feifan.bp.R;
+import com.feifan.bp.UserProfile;
 import com.feifan.bp.base.BaseActivity;
+import com.feifan.bp.widget.FloatingActionButton;
+import com.feifan.bp.widget.ObservableScrollView;
+import com.feifan.bp.widget.SelectPopWindow;
 
-public class BrowserTabActivity extends BaseActivity {
+public class BrowserTabActivity extends BaseActivity implements BrowserFragment.OnTitleReceiveListener{
     private BrowserTabPagerAdapter pagerAdapter;
     private ViewPager viewPager;
 
     private TabLayout tabLayout;
     private String tabTitles[];
-    private String url;
     private String arryStatus[];
     private BrowserTabItem[] arryTabItem;
+
+    private ObservableScrollView mScrollView;
+    private FloatingActionButton fab;
+    private SelectPopWindow mPopWindow;
+    private View mShadowView;
+    private int lastSelectPos = 0;
+
+    private boolean mShowFab = false;
+    private String sUrl;
+    private String mUrl;
+    private String mStoreId;
 
     /**
      * 参数键名称－URL
@@ -31,7 +48,7 @@ public class BrowserTabActivity extends BaseActivity {
 
 
     public static void startActivity(Context context, String url) {
-        startActivity(context, url,null, null);
+        startActivity(context, url, null, null);
     }
 
     /**
@@ -53,24 +70,76 @@ public class BrowserTabActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.actvity_browser_tab);
-        viewPager = (ViewPager) findViewById(R.id.viewpager);
-        // 载入网页
-        url = getIntent().getStringExtra(EXTRA_KEY_URL);
+        initViews();
+
+        mUrl = getIntent().getStringExtra(EXTRA_KEY_URL);
         arryStatus = getIntent().getStringArrayExtra(EXTRA_KEY_STATUS);
         tabTitles = getIntent().getStringArrayExtra(EXTRA_KEY_TITLES);
-
-        pagerAdapter = new BrowserTabPagerAdapter(getSupportFragmentManager(),tabTitles,url,arryStatus);
-        viewPager.setAdapter(pagerAdapter);
-        viewPager.setOffscreenPageLimit(0);
-        tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
-        tabLayout.setupWithViewPager(viewPager);
         arryTabItem = new BrowserTabItem[tabLayout.getTabCount()];
+
+        if(mShowFab){
+            mStoreId = UserProfile.getInstance().getStoreId(lastSelectPos);
+            sUrl = addStoreId(mUrl,mStoreId);
+        }else{
+            sUrl = mUrl;
+        }
+        loadWeb(sUrl);
+    }
+
+    private void initViews() {
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
         if(tabTitles.length>4){
             tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
         } else{
             tabLayout.setTabMode(TabLayout.MODE_FIXED);
         }
 
+        mScrollView = (ObservableScrollView) findViewById(R.id.scrollView);
+        mShadowView = findViewById(R.id.shadowView);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.attachToScrollView(mScrollView);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectMenu();
+            }
+        });
+    }
+
+    private void loadWeb(String url){
+        pagerAdapter = new BrowserTabPagerAdapter(getSupportFragmentManager(),tabTitles,url,arryStatus);
+        viewPager.setAdapter(pagerAdapter);
+        viewPager.setOffscreenPageLimit(0);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void selectMenu() {
+
+        mPopWindow = new SelectPopWindow(BrowserTabActivity.this, lastSelectPos);
+        mShadowView.setVisibility(View.VISIBLE);
+        mShadowView.startAnimation(AnimationUtils.loadAnimation(BrowserTabActivity.this, R.anim.pop_bg_show));
+        mPopWindow.showAtLocation(BrowserTabActivity.this.findViewById(R.id.fab),
+                Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 0, 0);
+        mPopWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+            @Override
+            public void onDismiss() {
+                mShadowView.startAnimation(AnimationUtils.loadAnimation(BrowserTabActivity.this, R.anim.pop_bg_hide));
+                mShadowView.setVisibility(View.INVISIBLE);
+                if (lastSelectPos != mPopWindow.getSelectPos()) {
+                    lastSelectPos = mPopWindow.getSelectPos();
+                    mStoreId = UserProfile.getInstance().getStoreId(lastSelectPos);
+                    sUrl = addStoreId(mUrl, mStoreId);
+                    loadWeb(sUrl);
+                }
+                mPopWindow.dismiss();
+            }
+        });
+    }
+
+    private String addStoreId(String url,String storeId){
+        String[] urls = url.split("\\?");
+        return urls[0]+"?storeId"+storeId+"&"+urls[1];
     }
 
     @Override
@@ -78,4 +147,14 @@ public class BrowserTabActivity extends BaseActivity {
         return true;
     }
 
+    @Override
+    public void OnTitleReceiveListener(String title) {
+        if(title.equals(getString(R.string.index_history_text))||
+                title.equals(getString(R.string.index_order_text))||
+                title.equals(getString(R.string.browser_staff_list))){
+            fab.setVisibility(View.VISIBLE);
+        }else{
+            fab.setVisibility(View.GONE);
+        }
+    }
 }
