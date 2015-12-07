a8d85afd3e07402246d51cec018e02da74619c6f
xuchunlei3 <xuchunlei3@wanda.cn>
2015-12-07 11:59:23 +0800

    Verify menu Right
    Change-Id: I93efecbc27eb6d1c0c30935523f87a161231edc7

--- app/src/main/java/com/feifan/bp/browser/BrowserMatcher.java
+++ app/src/main/java/com/feifan/bp/browser/BrowserMatcher.java
@@ -8,6 +8,7 @@ import com.feifan.bp.LaunchActivity;
 import com.feifan.bp.R;
 import com.feifan.bp.UserProfile;
 import com.feifan.bp.Utils;
+import com.feifan.bp.envir.EnvironmentManager;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -27,6 +28,9 @@ public class BrowserMatcher {
     // 顶部菜单项集合
     private static final Map<String, MenuInfo> sMenuStore;
 
+    // 顶部菜单权限集合
+    private static final Map<String, String> sRightStore;
+
     static {
         sMenuStore = new HashMap<String, MenuInfo>();
         sMenuStore.put(Utils.getString(R.string.browser_staff_list), new MenuInfo(R.id.menu_staff_add, R.mipmap.menu_ic_add));
@@ -34,6 +38,9 @@ public class BrowserMatcher {
         sMenuStore.put(Utils.getString(R.string.index_commodity_text), new MenuInfo(R.id.menu_commodity_add, R.mipmap.menu_ic_add));
         sMenuStore.put(Utils.getString(R.string.browser_commodity_desc), new MenuInfo(R.id.menu_picture_add, R.mipmap.menu_ic_pict));
         sMenuStore.put(Utils.getString(R.string.index_refund_text), new MenuInfo(R.id.menu_refund_start,Constants.NO_INTEGER, R.string.refund_menu_start_text));
+
+        sRightStore = new HashMap<String, String>();
+        sRightStore.put(Utils.getString(R.string.index_refund_text), EnvironmentManager.getAuthFactory().getRefundRightId());
     }
 
     /**
@@ -74,7 +81,22 @@ public class BrowserMatcher {
             return null;
         }
 
-        return sMenuStore.get(title);
+        return sMenuStore.get(getRightTitle(title));
+    }
+
+    /**
+     * 获取权限认证过的Title
+     * @param title
+     * @return
+     */
+    private String getRightTitle(String title) {
+        String right = sRightStore.get(title);
+        if(right != null) {        //需要进行权限认证
+            if(!UserProfile.getInstance().getRightString().contains(right)) {
+                return null;
+            }
+        }
+        return title;
     }
 
 }
