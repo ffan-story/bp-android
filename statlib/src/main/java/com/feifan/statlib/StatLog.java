5a1904400b3944739ee8713c88b1677b7074e48b
xuchunlei3 <xuchunlei3@wanda.cn>
2015-12-09 14:34:32 +0800

    fix exception in StatLog when msg = null
    Change-Id: I9f0397cc222e6d6d624746e3bbb3cd672430c005

--- statlib/src/main/java/com/feifan/statlib/StatLog.java
+++ statlib/src/main/java/com/feifan/statlib/StatLog.java
@@ -10,7 +10,9 @@ import android.util.Log;
 class StatLog {
 
     public static void d(String tag, String msg) {
-        Log.d(tag, msg);
+        if(msg != null) {
+            Log.d(tag, msg);
+        }
     }
 
     public static void i(String tag, String msg) {
@@ -21,11 +23,14 @@ class StatLog {
 //        if (UmsConstants.DebugLevel == LogLevel.Warn
 //                || UmsConstants.DebugLevel == LogLevel.Error)
 //            return;
-
-        Log.i(tag, msg);
+        if (msg != null) {
+            Log.i(tag, msg);
+        }
     }
 
     public static void e(String tag, String msg) {
-        Log.e(tag, msg);
+        if(msg != null) {
+            Log.e(tag, msg);
+        }
     }
 }
