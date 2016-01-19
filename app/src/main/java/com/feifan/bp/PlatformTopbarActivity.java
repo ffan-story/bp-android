a63560c3b45981da7ab37bc75c7300d663727c4a
xuchunlei3 <xuchunlei3@wanda.cn>
2016-01-19 09:42:17 +0800

    fix no title bug
    Change-Id: Ia2d08f7b25221e4b13352fb56090a389089074f8

--- app/src/main/java/com/feifan/bp/PlatformTopbarActivity.java
+++ app/src/main/java/com/feifan/bp/PlatformTopbarActivity.java
@@ -157,9 +157,10 @@ public class PlatformTopbarActivity extends PlatformBaseActivity implements OnFr
         String title = null;
         if(args != null) {
             title = args.getString(Constants.EXTRA_KEY_TITLE);
+            mCenterTitle.setText(title);
+            mToolbar.setVisibility(title == null ? View.GONE : View.VISIBLE);
         }
-        mCenterTitle.setText(title);
-        mToolbar.setVisibility(title == null ? View.GONE : View.VISIBLE);
+
     }
 
     @Override
