09f709d53ce8abadb82d5df3acbe7c28d5813479
RogerFong13 <fangke3@wanda.cn>
2016-02-26 12:13:59 +0800

    fix system alterdialog bugs when home key is pressed
    Change-Id: I26ed27fe075f0e2c61eabe6cf3e8136dda6a3eb7

--- app/src/main/java/com/feifan/bp/LaunchActivity.java
+++ app/src/main/java/com/feifan/bp/LaunchActivity.java
@@ -8,7 +8,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -75,8 +74,8 @@ public class LaunchActivity extends PlatformBaseActivity implements OnFragmentIn
         mFragments.add(SettingsFragment.newInstance());
 
         // 加载标题栏
-        mToolbar = (Toolbar)findViewById(R.id.head_bar);
-        mCenterTitle = (TextView)mToolbar.findViewById(R.id.header_center_title);
+        mToolbar = (Toolbar) findViewById(R.id.head_bar);
+        mCenterTitle = (TextView) mToolbar.findViewById(R.id.header_center_title);
 //        initHeader(mToolbar);
 
         // 初始化视图
@@ -116,9 +115,9 @@ public class LaunchActivity extends PlatformBaseActivity implements OnFragmentIn
         verifyContent();
 
         // 获取未读提示状态
-        if(UserProfile.getInstance().isStoreUser()){
+        if (UserProfile.getInstance().isStoreUser()) {
             storeId = UserProfile.getInstance().getAuthRangeId();
-        }else {
+        } else {
             merchantId = UserProfile.getInstance().getAuthRangeId();
         }
         HomeCtrl.getUnReadtatus(merchantId, storeId, USER_TYPE, new Response.Listener<ReadMessageModel>() {
@@ -168,8 +167,8 @@ public class LaunchActivity extends PlatformBaseActivity implements OnFragmentIn
 
     @Override
     public void retryRequestNetwork() {
-        if(mCurrentFragment instanceof MessageFragment){
-            ((MessageFragment)mCurrentFragment).updateData();
+        if (mCurrentFragment instanceof MessageFragment) {
+            ((MessageFragment) mCurrentFragment).updateData();
         }
     }
 
@@ -191,8 +190,8 @@ public class LaunchActivity extends PlatformBaseActivity implements OnFragmentIn
             showHome(true);
         } else if (from.equals(SettingsFragment.class.getName())) {//设置界面
             if (to.equals(LaunchActivity.class.getName())) {
-               startActivity(buildIntent(this));
-            }else{
+                startActivity(buildIntent(this));
+            } else {
                 PlatformTopbarActivity.startActivity(this, to);
             }
         } else if (from.equals(ForgetPasswordFragment.class.getName())) {
@@ -207,7 +206,7 @@ public class LaunchActivity extends PlatformBaseActivity implements OnFragmentIn
                 } else {
                     Utils.showShortToast(this, R.string.error_message_text_offline, Gravity.CENTER);
                 }
-            }else if(to.equals(CheckManageFragment.class.getName())){
+            } else if (to.equals(CheckManageFragment.class.getName())) {
                 if (Utils.isNetworkAvailable(this)) {
                     Intent intent = new Intent(this, PlatformTopbarActivity.class);
                     intent.putExtra(OnFragmentInteractionListener.INTERATION_KEY_TO, CheckManageFragment.class.getName());
@@ -215,7 +214,7 @@ public class LaunchActivity extends PlatformBaseActivity implements OnFragmentIn
                 } else {
                     Utils.showShortToast(this, R.string.error_message_text_offline, Gravity.CENTER);
                 }
-            } else if(to.equals(IndexSalesManageFragment.class.getName())){
+            } else if (to.equals(IndexSalesManageFragment.class.getName())) {
                 if (Utils.isNetworkAvailable(this)) {
                     Intent intent = new Intent(this, PlatformTopbarActivity.class);
                     intent.putExtra(OnFragmentInteractionListener.INTERATION_KEY_TO, IndexSalesManageFragment.class.getName());
@@ -223,18 +222,18 @@ public class LaunchActivity extends PlatformBaseActivity implements OnFragmentIn
                 } else {
                     Utils.showShortToast(this, R.string.error_message_text_offline, Gravity.CENTER);
                 }
-            }else if (to.equals(MarketingHomeFragment.class.getName())){//营销分析 二期
+            } else if (to.equals(MarketingHomeFragment.class.getName())) {//营销分析 二期
                 if (Utils.isNetworkAvailable(this)) {
                     Intent intent = new Intent(this, PlatformTopbarActivity.class);
                     intent.putExtra(OnFragmentInteractionListener.INTERATION_KEY_TO, MarketingHomeFragment.class.getName());
-                    intent.putExtra(Constants.EXTRA_KEY_TITLE,getString(R.string.sale_anal));
+                    intent.putExtra(Constants.EXTRA_KEY_TITLE, getString(R.string.sale_anal));
                     startActivity(intent);
                 } else {
                     Utils.showShortToast(this, R.string.error_message_text_offline, Gravity.CENTER);
                 }
             } else if (to.equals(BrowserTabActivity.class.getName())) {
                 openTabBrowser(args);
-            } else{
+            } else {
                 openBrowser(args.getString(BrowserActivity.EXTRA_KEY_URL));
             }
         } else if (from.equals(UserInfoFragment.class.getName())) {
@@ -252,7 +251,7 @@ public class LaunchActivity extends PlatformBaseActivity implements OnFragmentIn
 
     @Override
     public void onStatusChanged(boolean flag) {
-        if(flag) {
+        if (flag) {
             mMessageTab.showBadger();
         } else {
             mMessageTab.hideBadger();
@@ -274,7 +273,7 @@ public class LaunchActivity extends PlatformBaseActivity implements OnFragmentIn
         // 改变标题
         Bundle args = fragment.getArguments();
         String title = null;
-        if(args != null) {
+        if (args != null) {
             title = args.getString(Constants.EXTRA_KEY_TITLE);
         }
         mCenterTitle.setText(title);
@@ -306,7 +305,7 @@ public class LaunchActivity extends PlatformBaseActivity implements OnFragmentIn
     }
 
     // 检验内容
-    private void verifyContent(){
+    private void verifyContent() {
         if (UserCtrl.getStatus() == UserCtrl.USER_STATUS_LOGOUT) { //登出状态
             showLogin();
         }
@@ -339,7 +338,6 @@ public class LaunchActivity extends PlatformBaseActivity implements OnFragmentIn
 
     @Override
     public void onBackPressed() {
-        Utils.dismissLoginDialog();
         if (mCurrentFragment != null
                 && mCurrentFragment instanceof FeedBackFragment
                 || mCurrentFragment instanceof HelpCenterFragment) {
@@ -347,13 +345,5 @@ public class LaunchActivity extends PlatformBaseActivity implements OnFragmentIn
         } else {
             super.onBackPressed();
         }
-    }
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if(keyCode == event.KEYCODE_HOME){
-            Utils.dismissLoginDialog();
-        }
-        return super.onKeyDown(keyCode, event);
     }
 }
---
+++ app/src/main/java/com/feifan/bp/widget/SystemAlterDialog.java
@@ -0,0 +1,46 @@
+package com.feifan.bp.widget;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.view.WindowManager;
+
+import com.feifan.bp.LaunchActivity;
+import com.feifan.bp.R;
+import com.feifan.bp.UserProfile;
+
+/**
+ * Created by Frank on 16/2/26.
+ */
+public class SystemAlterDialog {
+
+    private static AlertDialog mDialog = null;
+
+    public static AlertDialog getInstance(final Context context, String msg) {
+        if (mDialog == null) {
+            mDialog = new AlertDialog.Builder(context)
+                    .setMessage(msg)
+                    .setCancelable(false)
+                    .setPositiveButton(R.string.common_confirm, new DialogInterface.OnClickListener() {
+
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                            UserProfile.getInstance().clear();
+                            Intent intent = LaunchActivity.buildIntent(context);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+                            context.startActivity(intent);
+                        }
+                    }).create();
+            mDialog.getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);//设置系统级别AlterDialog
+        }
+        return mDialog;
+    }
+
+    public static void dismissSystemDialog() {
+        if (mDialog != null && mDialog.isShowing()) {
+            mDialog.dismiss();
+        }
+    }
+}
