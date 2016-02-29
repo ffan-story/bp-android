b5bc99e5f2309856c2fb847b8af2307aeb9b37b7
mengmeng <mengmeng5@wanda.cn>
2016-02-29 15:45:56 +0800

    提交crash config的代码，可以正常提交
    提交crash config的代码，可以正常提交
    
    Change-Id: Ie125b3edaaf6051368e90b53012a0fa84ddba5fe
    Signed-off-by: mengmeng <mengmeng5@wanda.cn>

--- libworks/bpcrash/src/com/bp/crash/BPCrashConfig.java
+++ libworks/bpcrash/src/com/bp/crash/BPCrashConfig.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.text.TextUtils;
+import android.util.Log;
 
 import com.wanda.crashsdk.BuildConfig;
 import com.wanda.crashsdk.core.data.SDKConfig;
@@ -32,16 +33,15 @@ public class BPCrashConfig implements IConfiguration {
     private static final String VERSION_CODE = "FFClientVersion";
     private static final String CLIENT_TYPE = "FFClientType";
     private static final String SERVER_API_VERSION = "version";
-
     private static final String KEY_APP_ID = "AppId";
     //BP is value 2 ,ffan client is 1
     private static final String VALUE_APP_ID = "2";
-
     private static final String CRASH_PRODUCT_SERVER = "http://crash.ffan.com";
     private static final String CRASH_TEST_SERVER = "http://crash.sit.ffan.com";
 
     private static String mChannel;
     private String mUid;
+    private Context mContext;
 
 
     @Override
@@ -71,11 +71,12 @@ public class BPCrashConfig implements IConfiguration {
 
     @Override
     public AppBaseInfo getAppInfo(Context context) {
+        mContext = context;
         return constructAppBaseInfo(context);
     }
 
     public void setUid(String uid){
-
+        mUid = uid;
     }
 
     @Override
@@ -83,16 +84,30 @@ public class BPCrashConfig implements IConfiguration {
         return true;
     }
 
-    public static String getServerUrl() {
-        if(BuildConfig.DEBUG) {
+    public static String getServerUrl(boolean isDebug) {
+        if(isDebug) {
             return CRASH_TEST_SERVER;
         } else {
             return CRASH_PRODUCT_SERVER;
         }
     }
 
+
+    private boolean isApkDebugable(Context context) {
+        try {
+            ApplicationInfo info= context.getApplicationInfo();
+            return (info.flags&ApplicationInfo.FLAG_DEBUGGABLE)!=0;
+        } catch (Exception e) {
+
+        }
+        return true;
+    }
+
     private String getBaseUrl() {
-        return getServerUrl();
+        String url = getServerUrl(isApkDebugable(mContext));
+        Log.e("BuildConfig.DEBUG",String.valueOf(isApkDebugable(mContext)));
+        Log.e("crash url",url);
+        return url;
     }
 
     /**
@@ -108,12 +123,12 @@ public class BPCrashConfig implements IConfiguration {
         @Override
         public HttpParams getHttpParams() {
             HttpParams params = new HttpParams();
-
             params.put(UDID,CrashUtil.getMyUUID(mContext));
             params.put(IMEI, CrashUtil.getMyUUID(mContext));
             params.put(CLIENT_TYPE, CLIENT_TYPE_VALUE);
             params.put(VERSION_CODE,
                     String.valueOf(SystemUtil.getVersionCode(mContext)));
+            Log.e("versioncode",String.valueOf(SystemUtil.getVersionCode(mContext)));
             params.put(SERVER_API_VERSION, SERVER_VERSION);
             params.put(KEY_APP_ID, VALUE_APP_ID);
             return params;
@@ -122,14 +137,13 @@ public class BPCrashConfig implements IConfiguration {
 
     private AppBaseInfo constructAppBaseInfo(final Context context) {
         String packageName = context == null ? "com.feifan.bp" : context.getPackageName();
-
         String userId = mUid;
         // init channel
         if (TextUtils.isEmpty(mChannel)) {
             try {
                 ApplicationInfo appInfo = context.getPackageManager()
                         .getApplicationInfo(packageName, PackageManager.GET_META_DATA);
-                mChannel = appInfo.metaData.getString("CHANNEL");
+                mChannel = appInfo.metaData.getString("UMENG_CHANNEL");
             } catch (PackageManager.NameNotFoundException e) {
                 e.printStackTrace();
             }
@@ -138,11 +152,15 @@ public class BPCrashConfig implements IConfiguration {
         AppBaseInfo info = new AppBaseInfo();
         info.mDeviceId = CrashUtil.getMyUUID(context);
         info.mIMEI = SDKConfig.getIMEI();
-        info.mAppId = APPID;
+        info.mAppId = VALUE_APP_ID;
         info.mPackageName = packageName;
         info.mUserId = userId;
         info.mChannel = mChannel;
-        info.mBuildType = BuildConfig.BUILD_TYPE;
+        if(isApkDebugable(context)) {
+            info.mBuildType = "debug";
+        }else {
+            info.mBuildType = "release";
+        }
         return info;
     }
 }
